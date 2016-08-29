f7471a3fa12c9abe41ea30cf85f22b5bf9a5fd5f
clinechas <charles.cline@gmail.com>
2016-08-29 09:29:07 -0400

    Update O365_Licensing1.ps1

--- O365_Licensing1.ps1
+++ O365_Licensing1.ps1
@@ -1,2 +1,3 @@
+#Connect to Office 365 tenant, get list of licensed users and export to csv.
 ﻿connect-msolservice
 Get-MsolUser -all | Where-Object { $_.isLicensed -eq "TRUE" } | Export-Csv c:\_scripts\unlicensed.csv
