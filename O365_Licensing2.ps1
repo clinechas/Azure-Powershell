4d9be81afd77cdd3202bf883352126c973bad2ee
clinechas <charles.cline@gmail.com>
2016-08-31 16:33:12 -0400

    Update O365_Licensing2.ps1

--- O365_Licensing2.ps1
+++ O365_Licensing2.ps1
@@ -1,2 +1,3 @@
+#Connect to Office 365 tenant, get list of licensed users, license type,  and export to csv
 connect-msolservice
 Get-MSOLUser -All | % { $user=$_; $_.Licenses | Select {$user.displayname},AccountSKuid } | Export-CSV c:\licensetype.csv -NoTypeInformation
